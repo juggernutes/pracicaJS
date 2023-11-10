dc6a7b8327f751d0ae58cb569572dd43ae7faa53
Edgar Antonio Gomez Lopez <115048746+juggernutes@users.noreply.github.com>
2023-11-10 15:17:38 -0800

    Index

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-document.getElementById("formulario").addEventListener("submit", function(e){
+let peliculas = [];
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    if (localStorage.getItem("peliculas")) {
+        peliculas = JSON.parse(localStorage.getItem("peliculas"));
+        peliculas.forEach(function (pelicula) {
+            agregarTabla(pelicula);
+        });
+    }
+});
+
+document.getElementById("formulario").addEventListener("submit", function (e) {
     e.preventDefault();
-    let p[eliculas] = [];
+
     var pelicula = {
         id: getId(),
         titulo: document.getElementById("titulo").value,
@@ -9,19 +21,24 @@ document.getElementById("formulario").addEventListener("submit", function(e){
         genero: document.getElementById("genero").value,
         director: document.getElementById("director").value,
         sinopsis: document.getElementById("sinopsis").value,
-    }
+    };
+
     peliculas.push(pelicula);
+
+    
+    localStorage.setItem("peliculas", JSON.stringify(peliculas));
+
     agregarTabla(pelicula);
-    e.target.reset();
+    limpiar();
+});
 
-function agregarTabla(pelicula){
-
+function agregarTabla(pelicula) {
     var tbody = document.getElementById("cuerpoTabla");
     var fila = document.createElement("tr");
 
-    for (var key in pelicula){
+    for (var key in pelicula) {
         var td = document.createElement("td");
-        td.textContent = pelicula[key]
+        td.textContent = pelicula[key];
         fila.appendChild(td);
     }
 
@@ -31,9 +48,19 @@ function agregarTabla(pelicula){
     btnEliminar.className = "btn btn-danger";
     td.appendChild(btnEliminar);
     fila.appendChild(td);
-    btnEliminar.onclick = function(){
+    btnEliminar.onclick = function () {
+        
+        var index = peliculas.findIndex(function (p) {
+            return p.id === pelicula.id;
+        });
+        peliculas.splice(index, 1);
+
+        
+        localStorage.setItem("peliculas", JSON.stringify(peliculas));
+
+        
         tbody.removeChild(this.parentNode.parentNode);
-    }
+    };
 
     var td = document.createElement("td");
     var btnEditar = document.createElement("button");
@@ -41,14 +68,15 @@ function agregarTabla(pelicula){
     btnEditar.className = "btn btn-success";
     td.appendChild(btnEditar);
     fila.appendChild(td);
-    btnEditar.onclick = function(){
+    btnEditar.onclick = function () {
+        
         Editar(pelicula.id);
     };
 
     tbody.appendChild(fila);
-}})
+}
 
-function limpiar(){ 
+function limpiar() {
     document.getElementById("titulo").value = "";
     document.getElementById("anio").value = "";
     document.getElementById("duracion").value = "";
@@ -57,41 +85,70 @@ function limpiar(){
     document.getElementById("sinopsis").value = "";
 }
 
-function btnAgregar(){
-    btn1.textContent = "Agregar";
-    btn1.className = "btn btn-primary";
-    btn1.onclick = function(){
-        agregarTabla(pelicula);
-        limpiar();
-    }
-}
+function getId() {
+    let count;
 
-function getId(){
-    let count, id;
-
-    if(localStorage.getItem("id")){
+    if (localStorage.getItem("id")) {
         count = parseInt(localStorage.getItem("id")) + 1;
-    }else{
+    } else {
         count = 1;
     }
 
     localStorage.setItem("id", count);
-    id = "ID" + count;
-    return id;
+    return "ID" + count;
 }
 
-function Editar(id){
-    var pelicula = peliculas.find(function(pelicula){
+function Editar(id) {
+    var pelicula = peliculas.find(function (pelicula) {
         return pelicula.id === id;
     });
-    btn1.textContent = "Editar";
-    btn1.className = "btn btn-success";
+
+    
+    var index = peliculas.findIndex(function (p) {
+        return p.id === pelicula.id;
+    });
+    peliculas.splice(index, 1);
+
+    
+    localStorage.setItem("peliculas", JSON.stringify(peliculas));
+
+    
+    var tbody = document.getElementById("cuerpoTabla");
+    tbody.innerHTML = "";
+
+    
+    peliculas.forEach(function (p) {
+        agregarTabla(p);
+    });
+
+    
     document.getElementById("titulo").value = pelicula.titulo;
     document.getElementById("anio").value = pelicula.anio;
     document.getElementById("duracion").value = pelicula.duracion;
     document.getElementById("genero").value = pelicula.genero;
     document.getElementById("director").value = pelicula.director;
     document.getElementById("sinopsis").value = pelicula.sinopsis;
-    btnAgregar();
-    limpiar();
+
+    
+    btn1.textContent = "Editar";
+    btn1.className = "btn btn-success";
+    btn1.onclick = function () {
+        
+        peliculas.push(pelicula);
+
+        
+        localStorage.setItem("peliculas", JSON.stringify(peliculas));
+
+        
+        var tbody = document.getElementById("cuerpoTabla");
+        tbody.innerHTML = "";
+
+        
+        peliculas.forEach(function (p) {
+            agregarTabla(p);
+        });
+
+        
+        limpiar();
+    };
 }
